30be53d29b038efb2ab52cd7eb2664abda3ddfc3
Luis <lchoquepuma@unsa.edu.pe>
2025-11-20 11:17:28 -0600

    Pestaña inicial del juego (borrador)

---
+++ MainGUI.java
@@ -0,0 +1,7 @@
+public class MainGUI {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new VentanaBasica().setVisible(true);
+        });
+    }
+}
